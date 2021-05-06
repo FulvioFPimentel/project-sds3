aa440f2ab01e33ef3286e62907f0c36f4f82b870
FulvioFPimentel <fulviofp@hotmail.com>
2021-05-05 23:26:20 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.dsvendas.entities.Seller;
 
 public interface SellerRepository extends JpaRepository<Seller, Long> {
-
+	
 }
